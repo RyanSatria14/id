ed347d148d07512007cb4ad2c85b3d7bcea73702
RyanSatria14 <114641778+RyanSatria14@users.noreply.github.com>
2025-09-11 15:15:53 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 header('Content-Type: application/json');
 
 // Ganti dengan API key kamu
@@ -38,3 +43,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['prompt'])) {
     echo json_encode(['response' => $aiResponse]);
 }
 ?>
+
